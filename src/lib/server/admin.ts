e442e480e09648b14187b168ef918d0421235810
feuvpi <fredvpg@gmail.com>
2023-10-21 18:51:32 -0300

    feat server

---
+++ src/lib/server/admin.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-empty */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { getAuth } from 'firebase-admin/auth';
+import { getFirestore } from 'firebase-admin/firestore';
+import { CLIENT_EMAIL, PRIVATE_KEY, PROJECT_ID } from '$env/static/private'
+import pkg from 'firebase-admin';
+
+
+try {
+    pkg.initializeApp({
+        credential: pkg.credential.cert({
+            projectId: PROJECT_ID,
+            clientEmail: CLIENT_EMAIL,
+            privateKey: PRIVATE_KEY,
+        })
+
+
+    })
+} catch (err: any) {
+    if(!/already exists/u.test(err.message)) {
+        console.error('Firebase Admin Error: ', err.stack)
+    }
+}
+
+export const adminDB = getFirestore();
+export const adminAuth = getAuth();
