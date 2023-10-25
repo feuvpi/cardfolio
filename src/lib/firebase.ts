58df7e2ccba4975b9521f0572712617fef477eb7
feuvpi <fredvpg@gmail.com>
2023-10-24 23:52:59 -0300

    fb config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
@@ -12,40 +12,56 @@ import { writable } from "svelte/store";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-
+  apiKey: "AIzaSyCpiecbea0cfI8hi2jughPUA9hV9O3Vfcs", // this is a public project id, not security issue
+  authDomain: "auth-app-e21d2.firebaseapp.com",
+  projectId: "auth-app-e21d2",
+  storageBucket: "auth-app-e21d2.appspot.com",
+  messagingSenderId: "990208090387",
+  appId: "1:990208090387:web:e69330d7e414fb72d4fae6"
 };
 
 // Initialize Firebase
-//export const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
-export const app = initializeApp(firebaseConfig);
+let fb;
+if ( getApps().length <1 ) {
+  fb = initializeApp(firebaseConfig);
+} else {
+  fb = getApps()[0];
+}
+export const app = fb
+//export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
 
 
 
-function userStore(){
+/**
+ * @returns a store with the current firebase user
+ */
+function userStore() {
   let unsubscribe: () => void;
 
-  // -- set AuthStore to null 
-  if(!auth || !globalThis.window){
-    console.log("unsubscribe")
-    console.warn('Auth is not initialized or not in the browser');
+  if (!auth || !globalThis.window) {
+    console.warn('Auth is not initialized or not in browser');
     const { subscribe } = writable<User | null>(null);
     return {
-    subscribe,
-  }
+      subscribe,
+    }
   }
 
   const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+    console.log(auth)
     unsubscribe = onAuthStateChanged(auth, (user) => {
-      set(user);
-      
+      set(user); 
     });
-    console.log(unsubscribe)
+    console.log(unsubscribe())
     return () => unsubscribe();
+
   });
-  return { subscribe, };
+  console.log("aqui2")
+  return {
+    subscribe,
+  };
 }
 
 export const user = userStore();
