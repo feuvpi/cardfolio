82894dabadca477271d814e874e74e24a3b5464e
feuvpi <fredvpg@gmail.com>
2023-11-03 23:37:39 -0300

    login page

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -76,7 +76,7 @@
     />
     </form>
 
-    <div className="my-4">
+    <div class="my-4">
         {#if loading}
             <p class="text-secondary text-sm">Checking availability of @{username}...</p>
         {/if}
