e745b7ce6ad1427291d7f5a7d5ec3f673e38183e
feuvpi <fredvpg@gmail.com>
2023-11-15 22:31:53 -0300

    feat photo upload

--- src/routes/login/photo/+page.svelte
+++ src/routes/login/photo/+page.svelte
@@ -6,6 +6,7 @@
     
     let previewURL: string;
     let uploading = false;
+    $: href = `/${$userData?.username}/edit`;
 
     async function upload(e: any) {
         uploading = true;
@@ -28,15 +29,15 @@
 
 <h2 class="card-title">Upload picture</h2>
 <form action="" class="max-w-screen-md w-full">
-    <div class="form-control w-full max-w-xs my-10 mx-auto text-center">
+    <div class="form-control w-full max-w-xs my-2 mx-auto text-center">
         <img 
         src={previewURL ?? $userData?.photoURL ?? "/user.png"} 
         alt="photoURL"
         width="256"
-        height="256"
+        height="64"
         class="mx-auto"/>
-        <label for="photoURL" class="label">
-            <span class="label-text">Choose a file</span>
+        <label for="photoURL" class="label mt-8">
+            <span class="label-text">Choose a file:</span>
         </label>
         <input 
         on:change={upload}
@@ -44,6 +45,11 @@
         type="file"
         class="file-input file-input-bordered w-full max-w-xs"
         accept="image/png, image/jpeg, image/gif, image/webp"/>
+        {#if uploading}
+            <p>Uploading image...</p>
+            <progress class="progress progress-info w-56 mt-6"/>
+        {/if}
     </div>
 </form>
+<a {href} class="btn btn-primary"> Finish </a>
 </AuthCheck>
