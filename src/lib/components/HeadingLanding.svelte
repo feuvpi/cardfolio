6979a7f88f6a9a4bbc4478a6c8605854ee3d84df
feuvpi <fredvpg@gmail.com>
2023-11-24 19:24:19 -0300

    .

--- src/lib/components/HeadingLanding.svelte
+++ src/lib/components/HeadingLanding.svelte
@@ -1,7 +1,7 @@
 <nav class="bg-transparent w-full flex justify-center items-center mx-auto px-2 h-20">
-        <button type="button" class="flex mr-4 items-center border rounded-full  text-indigo-900 border-indigo-900">
-          <a class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full" href="#">
-            <div class="flex items-center relative cursor-pointer whitespace-nowrap">Logout</div>
+        <button type="button" class="flex mr-4 items-center border rounded-full border-2 text-purple-600 border-purple-600">
+          <a class="inline-block py-2 px-3 hover:bg-gray-800 rounded-full" href="#">
+            <div class="flex items-center relative cursor-pointer whitespace-nowrap">Log In</div>
           </a>
         </button>
 </nav>
