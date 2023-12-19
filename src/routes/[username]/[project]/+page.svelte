1a2ca50a0d9cd5ea52fe03d15e9e836028fe9f92
feuvpi <fredvpg@gmail.com>
2023-12-18 23:50:48 -0300

    .

--- src/routes/[username]/[project]/+page.svelte
+++ src/routes/[username]/[project]/+page.svelte
@@ -7,11 +7,17 @@
   export let projectTitle;
   export let projectDescription;
   export let projectImages;
+
+  function openImageModal(imageUrl) {
+      // Implement logic to showcase the image in a larger view
+      // This can be achieved through a modal or a lightbox effect
+      // Display the clicked image in a larger size
+    }
 </script>
 
 
 <div class="container mx-auto py-8">
-    <h1 class="text-3xl font-bold mb-4">{projectTitle}</h1>
+    <h1 class="text-3xl font-bold mb-4 font-mono">{projectTitle}</h1>
     <p class="text-gray-600 mb-6">{projectDescription}</p>
   
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
@@ -29,14 +35,7 @@
     <!-- You can create a modal component or use a library like 'svelte-dialog' for this -->
     <!-- When an image is clicked, it triggers the modal to display a larger version of the image -->
   </div>
-  <script>
-    // Function to open a modal or enlarge the image
-    function openImageModal(imageUrl) {
-      // Implement logic to showcase the image in a larger view
-      // This can be achieved through a modal or a lightbox effect
-      // Display the clicked image in a larger size
-    }
-  </script>
+
   <style>
     /* Tailwind classes or custom styles can be added here */
   </style>
